8e06527b1ee4362760a8067c069ac20ab22ad15d
Lex Myers <lex.myers@gmail.com>
2016-03-25 11:36:48 -0700

    implemented basic HTTP middleware

--- lexm/routes/login_routes.js
+++ lexm/routes/login_routes.js
@@ -3,22 +3,24 @@ var mongoose = require('mongoose');
 
 module.exports = (router, models) => {
   var jwt = require('jsonwebtoken');
+  var basicHTTP = require(__dirname + '/../lib/basic_http');
   var User = models.User;
-  router.post('/', (req, res) => {
-    console.log(req.headers);
-    var authorizationArray = req.headers.authorization.split(' ');
-    var method = authorizationArray[0];
-    var base64ed = authorizationArray[1];
-    var authArray = new Buffer(base64ed, 'base64').toString().split(':');
-    var name = authArray[0];
-    var password = authArray[1];
-    User.find({'name': name}, function(err, user) {
+  router.post('/', basicHTTP, (req, res) => {
+
+    // console.log(req.headers);
+    // var authorizationArray = req.headers.authorization.split(' ');
+    // var method = authorizationArray[0];
+    // var base64ed = authorizationArray[1];
+    // var authArray = new Buffer(base64ed, 'base64').toString().split(':');
+    // var name = authArray[0];
+    // var password = authArray[1];
+    User.find({'name': req.basicHTTP.name}, function(err, user) {
       if (err) {
         return res.json({status: 'error'});
       }
-      var valid = (!!user.length && user[0].compareHash(password));
+      var valid = (!!user.length && user[0].compareHash(req.basicHTTP.password));
       if(!valid) {
-        return res.json({status: 'failure', user: user, name: name, authArray: authArray, password: password});
+        return res.json({status: 'failure', basicHTTP: req.basicHTTP});
       }
       var newToken = user[0].generateToken();
       res.json({token: newToken});
